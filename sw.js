78c00ef2e07d2ed548a4946261081cadfd5c5e0e
luise-pixel <luise.maehl@web.de>
2026-09-24 21:41:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,4 @@
-const C='zahlungsplaner-v4-cache';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-self.addEventListener('push',e=>{
-  let d={title:'Zahlungsplaner',body:'Eine Zahlung ist bald fällig.'};
-  try{if(e.data)d=e.data.json()}catch(_){}
-  e.waitUntil(self.registration.showNotification(d.title||'Zahlungsplaner',{body:d.body||'',tag:d.tag||'zahlungsplaner-reminder',data:{url:'./index.html'}}));
-});
-self.addEventListener('notificationclick',e=>{
-  e.notification.close();
-  e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(cs=>{
-    for(const c of cs){if('focus'in c)return c.focus()}
-    return clients.openWindow(e.notification.data?.url||'./index.html');
-  }));
-});
+const C='zahlungsplaner-v5-cache';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png'])))}); 
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
