2e904ee671229dde055d1192021a002fb28ef2ed
luise-pixel <luise.maehl@web.de>
2026-09-24 21:34:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const C='zahlungsplaner-v4-cache';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icons/apple-touch-icon.png','./icons/icon-192.png','./icons/icon-512.png']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
 self.addEventListener('push',e=>{
   let d={title:'Zahlungsplaner',body:'Eine Zahlung ist bald fällig.'};
